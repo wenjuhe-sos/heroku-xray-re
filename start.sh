e1aeec0fca00099104d647e7edf975694ccd052e
wenjuhe-sos <77678695+wenjuhe-sos@users.noreply.github.com>
2021-05-08 18:08:00 +0800

    Add files via upload

--- start.sh
+++ start.sh
@@ -4,11 +4,11 @@
 # caddy
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" > /usr/share/caddy/robots.txt
 wget -qO  /usr/share/caddy/index.html $CADDYIndex
-wget -qO- $CONFIGCADDY | sed -e "s/\$AUUID/$AUUID/g" > /etc/caddy/caddy.json
+wget -qO- $CONFIGCADDY | sed -e "s/\$AUUID/$AUUID/g" > /caddy.json
 
 # xray
-wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$PORT/$PORT/g" > /etc/xray.json
+wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$PORT/$PORT/g" > /xray.json
 
 # start
-caddy start --config /etc/caddy/caddy.json
-/xray -config /etc/xray.json
+caddy start --config /caddy.json
+/xray -config /xray.json
